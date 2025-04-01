f97414d7b06d0200ae4c1734b78f65418050adb6
Alina Andreeva <runcannyvalley@gmail.com>
2025-04-01 19:07:44 +0300

    chore: update compileSdk version to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.memoria"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.memoria"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
